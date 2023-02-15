2153ab5e6c42ddfb332a98dec8ca7ab0091370d2
James Addison <siliconjungle.io@gmail.com>
2023-02-15 16:18:03 +1100

    update next config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,25 @@
-module.exports = {
+const DOCS_URL = 'http://localhost:3001'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ["ui"],
-};
+  transpilePackages: ['ui'],
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `/:path*`,
+      },
+      {
+        source: '/docs',
+        destination: `${DOCS_URL}/docs`,
+      },
+      {
+        source: '/docs/:path*',
+        destination: `${DOCS_URL}/docs/:path*`,
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
